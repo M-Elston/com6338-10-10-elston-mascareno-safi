acb0e7c6e64e4f10597b480a3e15f462fa25f6f1
Victoria Ortiz <ortiz.victoria@ufl.edu>
2025-07-26 05:38:54 -0400

    updated HTML and enhanced CSS styles for facts page and did facts.js to show random images of dogs

--- Javascript/facts.js
+++ Javascript/facts.js
@@ -1,1 +1,19 @@
-// Victoria's Javascript - Can remove this line
+const factsButton = document.getElementById('facts-button');
+const factsContainer = document.getElementById('facts-container');
+
+factsButton.addEventListener('click', function () {
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (imageData) {
+      const imageURL = imageData.message;
+
+      factsContainer.innerHTML = `
+        <img src="${imageURL}" alt="Cute dog photo" />
+      `;
+    })
+    .catch(function () {
+      factsContainer.textContent = 'Oops! Could not fetch a dog image.';
+    });
+});
