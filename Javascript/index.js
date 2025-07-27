699b9a31a61611199d42de7a2ed9a8b6d99b3824
Matt Elston <m.elston@ufl.edu>
2025-07-27 01:21:14 -0700

    Updated my page and shared files

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,1 +1,26 @@
 // My Javascript
+
+// Hero Banner API
+document.addEventListener('DOMContentLoaded', () => {
+    const hero = document.querySelector('.hero');
+    if (!hero) {
+        console.error('Hero section not found.');
+        return;
+    }
+
+    const savedImage = localStorage.getItem('heroDogImage');
+
+    if (savedImage) {
+        hero.style.backgroundImage = `url(${savedImage})`;
+    } else {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(response => response.json())
+            .then(data => {
+            hero.style.backgroundImage = `url(${data.message})`;
+            localStorage.setItem('heroDogImage', data.message);
+            })
+        .catch(err => console.error('Failed to fetch dog images:', err));
+        }
+});
+
+    
--- Javascript/shared.js
+++ Javascript/shared.js
@@ -1,1 +1,9 @@
 // Our Javascript - Can remove this line
+
+// Footer Automatic Year Copyright
+document.addEventListener('DOMContentLoaded', () => {
+    const footer = document.getElementById('site-footer');
+    const year = new Date().getFullYear();
+
+    footer.innerHTML = `&copy; ${year} Our Site`;
+})
