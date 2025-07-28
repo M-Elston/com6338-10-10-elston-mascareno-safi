f4eeaf7969f13f4fccd6f369e4c63d0cda2a64c7
Victoria Ortiz <ortiz.victoria@ufl.edu>
2025-07-28 05:01:52 -0400

    added local storage to save and display last dog image

--- Javascript/facts.js
+++ Javascript/facts.js
@@ -1,7 +1,12 @@
 //This is the JavaScript for generating a random dog image only on the facts page
+//This script also uses local storage to save and display the last fetched dog image
 
 const factsButton = document.getElementById('facts-button');
 const factsContainer = document.getElementById('facts-container');
+const savedImage = localStorage.getItem('dogImage');
+if (savedImage) {
+  factsContainer.innerHTML = `<img src="${savedImage}" alt="Cute dog photo" />`;
+}
 
 factsButton.addEventListener('click', function () {
   fetch('https://dog.ceo/api/breeds/image/random')
@@ -10,6 +15,7 @@ factsButton.addEventListener('click', function () {
     })
     .then(function (imageData) {
       const imageURL = imageData.message;
+      localStorage.setItem('dogImage', imageURL);
 
       factsContainer.innerHTML = `<img src="${imageURL}" alt="Cute dog photo" />`;
     })
